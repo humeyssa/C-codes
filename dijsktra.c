aa22d940b62155b00da76db5f53d2a59176def87
humeyssa <129295062+humeyssa@users.noreply.github.com>
2024-07-08 17:05:35 +0300

    Add files via upload
    Dijsktra algorithm c code

---
+++ dijsktra.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void dijsktra(struct Graph* g, int start, int target){
+	int i,j;
+	int visited[5]={0};
+	int dist[5];
+	for(i=0;i<5;i++){
+		dist[i]=999;
+	}
+	dist[start]=0;//baslangic icin mesafe 0
+	
+	int min =999;
+	for(j=0;j<4;j++){
+		if()
+	}
+	
+	
+}
+
+int main(){
+	
+	
+	return 0;
+}
